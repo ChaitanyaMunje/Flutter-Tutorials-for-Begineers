229811e437bed5f7f3043f5886749317623ce576
chaitanyamunje@gmail.com <Munje@1069>
2020-09-05 18:19:12 +0530

    Text Widget is described with all details.

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,30 @@ class HomePage extends StatelessWidget {
         child: Container(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text('My First Flutter App'),
+            child: Text('My First Flutter App',
+              style: TextStyle(fontSize: 20,color: Colors.blueAccent,fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,),
           ),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(shape: BoxShape.rectangle,color: Colors.red,
 
+              gradient: LinearGradient(colors: [
+                Colors.red,
+                Colors.yellowAccent
+              ]),
+
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 10,
+
+                )
+              ]
+
+
+          ),
 
         ),
       ),
